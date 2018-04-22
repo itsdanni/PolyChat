d359007069eb7571c701922d4f3d3ce89be47cf6
jshih7 <8042780+jshih7@users.noreply.github.com>
2018-04-22 10:50:19 -0400

    Added analyzer to utils

---
+++ server/api/utils/analyzer.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const rp = require('request-promise');
+
+// analysisType is either:
+// - 'languages' - detects language
+// - 'sentiment' - analyzes sentiment
+// - 'keyPhrases' - extracts key phrases
+//
+// documents is an object structured as follows:
+// { 'documents': [ { 'id': '0', 'language': 'languageCode, 'text': 'textToAnalyze' } ] }
+//
+module.exports.analyze = function(analysisType, documents) {
+
+	// Replace the accessKey string value with your valid access key.
+	const accessKey = '49b5f83e7b144136b5b85cb52294ddc0';
+
+	// Replace or verify the region.
+
+	// You must use the same region in your REST API call as you used to obtain your access keys.
+	// For example, if you obtained your access keys from the westus region, replace 
+	// "westcentralus" in the URI below with "westus".
+
+	// NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+	// a free trial access key, you should not need to change this region.
+	const endpoint = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0";
+	const path = '/' + analysisType;
+
+	const request_params = {
+        method: 'POST',
+        uri: endpoint + path,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : accessKey,
+        },
+        body: JSON.stringify(documents),
+        resolveWithFullResponse: true
+    };
+
+    const results_asPromise = rp(request_params)
+        .then((res) => {
+            console.log("IN ANALYTICS.JS: Response body: ", res.body)
+            const body_asJson= JSON.parse(res.body);
+            //const stringifiedBody = JSON.stringify(parsedBody, null, '  ');
+			//console.log(`IN ANALYTICS.JS: OUTPUT for ${analysisType}: `, body_asJson);
+            return body_asJson;
+        })
+        .catch((err) => console.log(err));
+
+    return results_asPromise;
+}
